93e8f8e7f3070bc1975221d70b8d8d49cb1aac02
ShaileshCC <chhalotreshailesh@gmail.com>
2026-07-27 17:42:47 +0530

    pdf service added

---
+++ backend/src/main/java/com/example/demo/service/impl/CarServiceImpl 2.java
@@ -0,0 +1,46 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.response.CarResponse;
+import com.example.demo.entity.base.Car;
+import com.example.demo.exception.error.ResourceNotFoundException;
+import com.example.demo.repository.CarRepository;
+import com.example.demo.service.CarService;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private final CarRepository repository;
+
+    public CarServiceImpl(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<CarResponse> getAllCars() {
+
+        List<Car> cars = repository.findAll();
+
+        if (cars.isEmpty()) {
+            throw new ResourceNotFoundException("No cars found.");
+        }
+
+        return cars.stream()
+                .map(CarResponse::fromEntity)
+                .toList();
+    }
+
+    @Override
+    public CarResponse getCarById(Integer carId) {
+
+        Car car = repository.findById(carId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Car not found with Id : " + carId));
+
+        return CarResponse.fromEntity(car);
+    }
+}
